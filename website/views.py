45669ac4112eb79d1b59220de2722d7669fed595
Chloe <ama1049@nyu.edu>
2022-11-08 17:50:27 -0500

    Half-finished login and register

--- website/views.py
+++ website/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
+import hashlib
+import datetime
+from django.db import models
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -15,6 +20,16 @@ def budget(request):
     return HttpResponse("Budget not implemented yet")
 
 def login(request):
+    if request.method == 'POST':
+        #verify all fields are valid
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            request.session['session_id'] = hashlib.sha256(str(str(user.id) + str(datetime.datetime.now())).encode('utf-8')).hexdigest()
+            request.session['create_time'] = datetime.datetime.now().timestamp()
+            request.session['username'] = username
+            return redirect('index')
     template = loader.get_template('website/login.html')
     context = {}
     return HttpResponse(template.render(context, request))
@@ -23,6 +38,23 @@ def logout(request):
     return HttpResponse("Logout not implemented yet")
 
 def register(request):
+    if request.method == 'POST':
+        #verify all fields are valid
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        if User.objects.filter(username=username).exists():
+            return HttpResponse("Username already exists")
+        if User.objects.filter(email=email).exists():
+            return HttpResponse("Email already exists")
+        if len(password) < 8:
+            return HttpResponse("Password must be at least 8 characters")
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return redirect('login')
+    template = loader.get_template('website/register.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
     return HttpResponse("Register not implemented yet")
 
 def settings(request):
